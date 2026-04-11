678b87d959bfbf16d96945c2d7c6790ca4a47632
KingTantin <ctranschel@outlook.de>
2026-04-11 22:20:59 +0200

    added best_score tracking and perfect games counter

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -3,7 +3,7 @@ import numpy as np
 from ppo_torch import Agent
 import matplotlib.pyplot as plt
 import time
-#changes since last commit: -add more Hyperparameters to reinforcement_learning -changed file paths to Snake_Ai  - fix drawing function  - better generation of apples
+
     
 
 if __name__ == '__main__':
@@ -19,20 +19,22 @@ if __name__ == '__main__':
     N = 1024
     batch_size = 128
     num_learning_epochs = 3
-    learning_rate = 0.0002
-    value_coef = 0.7
-    entropy_coef = 0.07
-    policy_clip = 0.3
+    learning_rate = 0.0003
+    value_coef = 0.8
+    entropy_coef = 0.05
+    policy_clip = 0.2
     num_games = 100000
 
 
     #Initializing variables
     score_history = []
     best_avg_score = 0
+    best_score = 0
     learn_iters = 0
     avg_score = 0
     n_steps = 0
     current_learning_step = 0
+    perfect_games = 0
 
     #for plotting
     episode_list = []
@@ -51,7 +53,7 @@ if __name__ == '__main__':
     
 
     agent = Agent(n_actions = n_actions, batch_size = batch_size, learning_rate = learning_rate, num_epochs = num_learning_epochs, input_dims = input_dims, policy_clip=policy_clip, value_coef=value_coef, entropy_coef=entropy_coef)
-    #agent.load_models('/home/constantin/Downloads/Coding_Projects/Artifical_Intelligence/Snake_AI/best_6x6_networks')
+    #agent.load_models('file_path')
     agent.load_models() #-> loads the files in netoworks
 
 
@@ -68,11 +70,10 @@ if __name__ == '__main__':
             
 
             observations_, reward, done = env.step(action)
-            
 
 
             n_steps += 1
-            score += np.mean(reward)
+            score += reward
 
             
 
@@ -86,8 +87,8 @@ if __name__ == '__main__':
                 episode_list.append(learn_iters)
                 avg_score_list.append(avg_score)
 
-            if episode % 2000000 == 0 and done == False:
-                env.draw_game()
+            if episode % 1000 == 0 and done == False:
+                env.draw_game(observations_)
                 
                 
 
@@ -100,6 +101,11 @@ if __name__ == '__main__':
 
 
 
+        if reward > 0:
+            perfect_games += 1
+        
+        if score > best_score:
+            best_score = score
 
         if avg_score > best_avg_score and episode > 100:
             best_avg_score = avg_score
@@ -108,7 +114,11 @@ if __name__ == '__main__':
                 current_learning_step = learn_iters
                 agent.save_models()
 
-        print(f'episode: {episode} \tscore: {round(score, 2)} \t avg_score: {round(avg_score, 2)} \t, best_avg_score: {round(best_avg_score, 2)} \t time_steps: {n_steps} \t learning_steps: {learn_iters}')
+
+        if perfect_games > 0:
+            print(f'episode: {episode} \tscore: {round(score, 2)} \t avg_score: {round(avg_score, 2)} \t, best_avg_score: {round(best_avg_score, 2)} \t learning_steps: {learn_iters} \t perfect_games: {perfect_games}')
+        else:
+            print(f'episode: {episode} \tscore: {round(score, 2)} \t avg_score: {round(avg_score, 2)} \t, best_avg_score: {round(best_avg_score, 2)} \t learning_steps: {learn_iters} \t best_score: {best_score}')
 
     plt.plot(episode_list, avg_score_list)
     plt.savefig('Snake_AI/Score_Graph.png')
