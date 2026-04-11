b13d76c94a5ea0eb5becf907fe8fd4debcc1e846
KingTantin <ctranschel@outlook.de>
2026-04-11 19:31:05 +0200

    added more Hyperparameters and fixed naming issue

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -2,8 +2,8 @@ import game as game
 import numpy as np
 from ppo_torch import Agent
 import matplotlib.pyplot as plt
-
-
+import time
+#changes since last commit: -add more Hyperparameters to reinforcement_learning -changed file paths to Snake_Ai  - fix drawing function  - better generation of apples
     
 
 if __name__ == '__main__':
@@ -19,8 +19,11 @@ if __name__ == '__main__':
     N = 1024
     batch_size = 128
     num_learning_epochs = 3
-    learning_rate = 0.0003
-    num_games = 10000
+    learning_rate = 0.0002
+    value_coef = 0.7
+    entropy_coef = 0.07
+    policy_clip = 0.3
+    num_games = 100000
 
 
     #Initializing variables
@@ -36,7 +39,7 @@ if __name__ == '__main__':
     avg_score_list = []
 
 
-    env = game.snake_environement(width, height, move_limit, block_size, visual=True)
+    env = game.snake_environment(width, height, move_limit, block_size, visual=False)
 
 
 
@@ -47,29 +50,25 @@ if __name__ == '__main__':
     input_dims = env.observation_space_shape()
     
 
-    agent = Agent(n_actions = n_actions, batch_size = batch_size, learning_rate = learning_rate, num_epochs = num_learning_epochs, input_dims = input_dims)
-    #agent.load_models('filepath')
-    #agent.load_models() -> loads the files in netoworks
+    agent = Agent(n_actions = n_actions, batch_size = batch_size, learning_rate = learning_rate, num_epochs = num_learning_epochs, input_dims = input_dims, policy_clip=policy_clip, value_coef=value_coef, entropy_coef=entropy_coef)
+    #agent.load_models('/home/constantin/Downloads/Coding_Projects/Artifical_Intelligence/Snake_AI/best_6x6_networks')
+    agent.load_models() #-> loads the files in netoworks
 
 
 
 
     for episode in range(num_games):
         observation = env.reset()
-
-        
-
-
         done = False
         score = 0
 
         while not done:
 
             action, prob, val = agent.choose_action(observation)
-
+            
 
             observations_, reward, done = env.step(action)
-
+            
 
 
             n_steps += 1
@@ -87,7 +86,7 @@ if __name__ == '__main__':
                 episode_list.append(learn_iters)
                 avg_score_list.append(avg_score)
 
-            if episode % 1000 == 0 and done == False:
+            if episode % 2000000 == 0 and done == False:
                 env.draw_game()
                 
                 
@@ -102,7 +101,7 @@ if __name__ == '__main__':
 
 
 
-        if avg_score > best_avg_score:
+        if avg_score > best_avg_score and episode > 100:
             best_avg_score = avg_score
 
             if current_learning_step < learn_iters:
@@ -112,4 +111,4 @@ if __name__ == '__main__':
         print(f'episode: {episode} \tscore: {round(score, 2)} \t avg_score: {round(avg_score, 2)} \t, best_avg_score: {round(best_avg_score, 2)} \t time_steps: {n_steps} \t learning_steps: {learn_iters}')
 
     plt.plot(episode_list, avg_score_list)
-    plt.savefig('Snake/Score_Graph.png')
+    plt.savefig('Snake_AI/Score_Graph.png')
